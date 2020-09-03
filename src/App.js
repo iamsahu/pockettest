c614e40e9bf1ef4bb472ed07fd97d2ca93bfb025
psahu6 <prafful.sahu1@gmail.com>
2020-09-03 19:24:43 +0530

    Token working

--- src/App.js
+++ src/App.js
@@ -1,37 +1,55 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-
+// async function GetToken(){
+//   fetch('https://wfjiolg53e.execute-api.eu-west-3.amazonaws.com/default/pocketAuth').then((r) => r.json())
+//     .then((data) => {
+//       console.log(data)
+//       var t = data['code'].split('=')
+//       // setCode(t[1])
+//       return t[1]
+//     })
+    
+// }
 
 function App() {
+  const [code, setCode] = useState('')
   // let pocket = require('pocket-api')
-  fetch('https://getpocket.com/v3/oauth/request', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-    },
-    body: JSON.stringify({consumer_key: '92104-32f15adc016c93919a53d671',redirect_uri:'https://google.com'})
-  })
-  .then(r => r.json())
-  .then((data) => {
-    console.log(data)
-              });
-// let consumer_key = '92104-32f15adc016c93919a53d671';
+  
 
-// let pocket = new PocketAPI(consumer_key);
+  function GetToken(){
+    fetch('https://wfjiolg53e.execute-api.eu-west-3.amazonaws.com/default/pocketAuth').then((r) => r.json())
+    .then((data) => {
+      console.log(data)
+      var t = data['code'].split('=')
+      setCode(t[1])
+      
+    })
+  }
 
-// pocket.getRequestToken()
-// .then((response) => {
-//     console.log(response)
-//     //returns request_token
-// })
+  function SendToAuthorize(){
+    var link="https://getpocket.com/auth/authorize?request_token="+code+"&redirect_uri=https://pockettest.vercel.app/"
+    window.open(link,'_blank')
+  }
+
+  function SendToAuto(){
+    var link="https://getpocket.com/auth/authorize?code="+code+"&consumer_key=92104-32f15adc016c93919a53d671"
+    fetch(link).then((r) => r.json())
+    .then((data) => {
+      console.log(data)
+      
+    })
+  }
   
 
   return (
     <div className="App">
-      <button >Click!</button>
+      <div>{code}</div>
+      <button onClick={GetToken}>Get Token!</button>
+      <button onClick={SendToAuthorize}>Click!</button>
+      <button onClick={SendToAuto}>Auto!</button>
     </div>
   );
 }
