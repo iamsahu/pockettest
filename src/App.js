943a253cf219d7aa2140ca3bdaffa615d4d711c6
psahu6 <prafful.sahu1@gmail.com>
2020-09-03 22:02:57 +0530

    test

--- src/App.js
+++ src/App.js
@@ -34,6 +34,19 @@ function App() {
     window.open(link,'_blank')
   }
 
+  function SendToAuto2(){
+    var requestOptions = {
+      method: 'POST',
+      // headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    fetch("https://getpocket.com/v3/oauth/authorize?consumer_key=92104-32f15adc016c93919a53d671&code="+code, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
   function SendToAuto(){
     
     var link="https://getpocket.com/v3/oauth/authorize?code="+code+"&consumer_key=92104-32f15adc016c93919a53d671"
@@ -92,7 +105,7 @@ function App() {
       <div>{code}</div>
       <button onClick={GetToken}>Get Token!</button>
       <button onClick={SendToAuthorize}>Click!</button>
-      <button onClick={SendToAuto}>Auto!</button>
+      <button onClick={SendToAuto2}>Auto!</button>
     </div>
   );
 }
